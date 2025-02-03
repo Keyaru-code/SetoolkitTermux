70321b5ee13cc817d75830af1309fc634394cb71
Anon4You <katilchora666@gmail.com>
2025-02-03 18:32:18 +0530

    setoolkit in termux using proot env

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SetoolkitTermux
 The Social-Engineer Toolkit (SET) repository from TrustedSec [termux-proot]
+
+Architecture : aarch64 
+
+## Installation
+git clone this fucking repository and navigate to *SetoolkitTermux* directorythen run `bash install.sh`
+
+
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+main() {
+  printf "\n 
+####################################
+ SOCIAL-ENGENIERING-TOOLKIT
+ IN TERMUX, BY : @ALIENKRISHN
+####################################
+   
+[*] installtion will take upto 700mb internet
+and 2gb storage...install it (yes/no)
+  "
+  read chs
+  if [[ $chs == yes ]]; then
+    echo "Installing setoolkit in termux..."
+  else
+    echo -e "Bye bye..."
+    sleep 2
+    exit 0
+  fi
+}
+
+checkarch() {
+  arch=$(uname -m)
+  if [[ $arch != aarch64 ]]; then
+    echo -e "Your device arch is not supported"
+    sleep 2
+    exit 1
+  fi
+}
+
+insdeps() {
+	printf "\n[*] Updating apt cache..."
+	apt update -y &> /dev/null
+	echo "\n [*] Checking for all required tools..."
+
+	for i in proot tar curl; do
+		if [ -e $PREFIX/bin/$i ]; then
+			echo "\n  • ${i} is OK"
+		else
+			echo "\nInstalling ${i}..."
+			apt install -y $i || 
+                        {
+				printf "\nERROR: check your internet connection or apt"
+				printf "\n Exiting...\n"
+				exit 1
+			}
+		fi
+	done
+	apt upgrade -y
+}
+
+inset() {
+  install -Dm700 $PREFIX/bin setoolkit
+  if [[ -d $PREFIX/share/setoolkit ]]; then
+    printf "\nHein..setoolkit is already installed"
+    exit 0
+  else
+    mkdir -p $PREFIX/share/setoolkit
+    echo -e "\e[32mDownloading setoolkit tar file please wait...\e[0m\n"
+    curl -LO https://github.com/Keyaru-code/SetoolkitTermux/releases/download/8.0.3/setoolkit.tar.xz
+    tar -xvJf setoolkit.tar.xz
+    rm -rf setoolkit.tar.xz
+  fi
+}
+main
+checkarch
+insdeps
+inset
+
